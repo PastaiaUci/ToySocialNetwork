66e9363abb8fb4da9ea39d9f8caf9ab4f71e9c2f
Razvan Miros <miros.razvan22@yahoo.com>
2022-01-13 17:21:36 +0200

    UpdateGUI

--- src/main/java/com/example/toysocialnetworkgui/Main.java
+++ src/main/java/com/example/toysocialnetworkgui/Main.java
@@ -43,9 +43,9 @@ public class Main extends Application {
         Repository<Tuple<Long,Long>, Friendship> friendshipDbRepository = null;
         Repository<Long, Message> messageDbRepository = null;
         try {
-            userDbRepository = new UserDbRepository("jdbc:postgresql://localhost:5432/postgres","postgres","postgres", new UserValidator());
-            friendshipDbRepository = new FriendshipsDbRepository("jdbc:postgresql://localhost:5432/postgres","postgres","postgres",new FriendshipValidator());
-            messageDbRepository = new MessageDbRepository("jdbc:postgresql://localhost:5432/postgres","postgres","postgres", new MessageValidator());
+            userDbRepository = new UserDbRepository("jdbc:postgresql://localhost:5432/academic","postgres","22adc#cJf6", new UserValidator());
+            friendshipDbRepository = new FriendshipsDbRepository("jdbc:postgresql://localhost:5432/academic","postgres","22adc#cJf6",new FriendshipValidator());
+            messageDbRepository = new MessageDbRepository("jdbc:postgresql://localhost:5432/academic","postgres","22adc#cJf6", new MessageValidator());
 
         }
         catch (FileError ex){
